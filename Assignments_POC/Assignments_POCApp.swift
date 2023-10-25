d4766fbbe938750a1c671a55f68dcbf7f3abb1d7
Arjunan371 <m.arjunan371@gmail.com>
2023-10-25 10:50:53 +0530

    Initial Commit

---
+++ Assignments_POC/Assignments_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignments_POCApp.swift
+//  Assignments_POC
+//
+//  Created by Arjunan on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignments_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignments_POCUITests/Assignments_POCUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Assignments_POCUITestsLaunchTests.swift
+//  Assignments_POCUITests
+//
+//  Created by Arjunan on 25/10/23.
+//
+
+import XCTest
+
+final class Assignments_POCUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
